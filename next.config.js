dabcc8c5a952aeae1eb6fd5425b4b2c9bac15f17
Stass7 <166198172+Stass7@users.noreply.github.com>
2025-05-25 09:17:46 +0300

    Remove custom error pages to fix Html import error on Render - use default Next.js error handling

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   images: {
     domains: ['i.ytimg.com', 'img.youtube.com'],
   },
-  // Отключаем статическую генерацию для отладки
-  trailingSlash: false,
-  generateEtags: false,
-  // Принудительно отключаем статическую генерацию для проблемных страниц
-  experimental: {
-    forceSwcTransforms: true,
-  },
-  // Отключаем генерацию статических ошибочных страниц
-  output: 'standalone',
 }
 
 module.exports = nextConfig 
